d576e58bc1721e3f2dabb315e621732dc749c4f6
SergioVillanuevaLagos <sergio.villanueva2101@alumnos.ubiobio.cl>
2025-06-02 00:40:55 -0400

    aaa
    aaa

--- src/app/services/beneficiary.service.ts
+++ src/app/services/beneficiary.service.ts
@@ -1,56 +1,34 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BeneficiaryService {
 
-    //private apiUrl = 'http://localhost:3000/users'; // Cambia Cuando termine el backend
+  private apiUrl = `${environment.apiUrl}/users`; // Ruta completa a tu endpoint
 
-    private users = [
-      { id: 1, nombres: 'Juan', apellidos:'Pérez', email: 'juan@example.com', telefono: '123456789',comuna:'Chillan',rut:'11.111.111-1' },
-      { id: 2, nombres: 'Ana', apellidos:'Gómez', email: 'ana@example.com', telefono: '987654321',comuna:'Chillan',rut:'22.222.222-2' }
-    ];
+  constructor(private http: HttpClient) {}
 
-    constructor() {}
+  getAllUsers(): Observable<any> {
+    return this.http.get(this.apiUrl);
+  }
 
-    // Obtener todos los usuarios
-    getAllUsers(): Observable<any> {
-      return of(this.users);  // Simula la respuesta de la API
-    }
+  getUserById(id: number): Observable<any> {
+    return this.http.get(`${this.apiUrl}/${id}`);
+  }
 
-    // Obtener un usuario por su ID
-    getUserById(id: number): Observable<any> {
-      const user = this.users.find(u => u.id === id);
-      return of(user);  // Devuelve el usuario con el ID especificado
-    }
+  createUser(user: any): Observable<any> {
+    return this.http.post(this.apiUrl, user);
+  }
 
-    // Crear un nuevo usuario
-    createUser(user: any): Observable<any> {
-      user.id = this.users.length + 1;  // Asigna un nuevo ID
-      this.users.push(user);  // Agrega el usuario a la lista
-      return of(user);  // Devuelve el usuario creado
-    }
+  updateUser(id: number, user: any): Observable<any> {
+    return this.http.put(`${this.apiUrl}/${id}`, user);
+  }
 
-    // Actualizar un usuario
-    updateUser(id: number, user: any): Observable<any> {
-      const index = this.users.findIndex(u => u.id === id);
-      if (index !== -1) {
-        this.users[index] = { ...this.users[index], ...user };  // Actualiza los datos
-      }
-      return of(this.users[index]);  // Devuelve el usuario actualizado
-    }
-
-    // Eliminar un usuario
-    deleteUser(id: number): Observable<any> {
-      const index = this.users.findIndex(u => u.id === id);
-      if (index !== -1) {
-        this.users.splice(index, 1);  // Elimina el usuario de la lista
-      }
-      return of({ message: 'Usuario eliminado' });  // Simula la eliminación
-    }
-
+  deleteUser(id: number): Observable<any> {
+    return this.http.delete(`${this.apiUrl}/${id}`);
+  }
 }
-
-
