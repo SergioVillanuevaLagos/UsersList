63c26b29634b8c1e1feac9308a06c6502baf5134
SergioVillanuevaLagos <sergio.villanueva2101@alumnos.ubiobio.cl>
2025-06-02 01:27:45 -0400

    Router arreglado para home
    se agrero la ruta home a router ouutler

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
+import { HomeComponent } from './views/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+// Importa tu componente Home aquí
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },  // carga home al acceder a la raíz
+  { path: 'home', component: HomeComponent },  // opcional si quieres que /home también funcione
+  // otras rutas
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
